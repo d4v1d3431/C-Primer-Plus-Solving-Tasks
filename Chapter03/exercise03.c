9fc44b007b595fdc19b7775987e6501af70a05e2
d4v1d3431 <41838236+d4v1d3431@users.noreply.github.com>
2018-09-03 15:27:10 +0400

    Create exercise03.c

---
+++ Chapter03/exercise03.c
@@ -0,0 +1,14 @@
+//C Primer Plus
+//Chapter 3 Exercise 3:
+//Write a program that sounds an alert and then prints the following text: Startled by the sudden sound, Sally shouted,
+//"By the Great Pumpkin, what was that!"
+
+#include <stdio.h>
+
+void main (void)
+{
+
+	printf("\a\n");
+	printf("Startled by the sudden sound, Sally shouted, \"By the Great Pumpkin, what was that!\"");
+
+}
